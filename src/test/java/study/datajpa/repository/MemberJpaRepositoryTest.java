91fe9efa4a582c42612f115fb70234fe2ab64649
한성길 <zaien24@naver.com>
2024-05-27 23:18:21 +0900

    섹션3 공통 인터페이스 적용

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,10 +1,13 @@
 package study.datajpa.repository;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,5 +28,34 @@ class MemberJpaRepositoryTest {
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        AssertionsForClassTypes.assertThat(findMember1).isEqualTo(member1);
+        AssertionsForClassTypes.assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        AssertionsForClassTypes.assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        AssertionsForClassTypes.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(findMember1);
+        memberJpaRepository.delete(findMember2);
+
+        long deletedCount = memberJpaRepository.count();
+        AssertionsForClassTypes.assertThat(deletedCount).isEqualTo(0);
     }
 }
