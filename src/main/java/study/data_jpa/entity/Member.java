15ac40f394eedbb1fa49c6498ebe613aba5ff0f9
yuyuhi <zaien24@naver.com>
2024-05-27 12:47:40 +0900

    섹션1 스프링 데이터 JPA와 DB 설정, 동작확인

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -1,9 +1,29 @@
 package study.data_jpa.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Entity
+@Getter
+@Setter
 public class Member {
 
-    
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    protected Member() {
+
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+
 }
