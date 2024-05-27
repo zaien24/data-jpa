2819f110c28716c4379e89f2c009aad723607973
한성길 <zaien24@naver.com>
2024-05-27 23:12:26 +0900

    섹션3 공통 인터페이스 설정

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,40 +1,7 @@
 package study.datajpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
-import study.datajpa.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        return em.createQuery("from Team", Team.class).getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count() {
-        return em.createQuery("select count(*) from Team", Long.class).getSingleResult();
-    }
-
+public interface TeamRepository extends JpaRepository {
 
 }
